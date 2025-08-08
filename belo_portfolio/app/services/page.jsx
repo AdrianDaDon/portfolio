40685f3a152e7536b162c334a49b0f6617ed2d08
Adrian <80717684+AdrianDaDon@users.noreply.github.com>
2025-08-08 12:30:45 +0200

    descriptive service

--- belo_portfolio/app/services/page.jsx
+++ belo_portfolio/app/services/page.jsx
@@ -9,23 +9,23 @@ const services = [
     num: "01",
     title: "Mobile Development",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nisi, eum, mollitia non error numquam sunt itaque magnam rerum quasi reprehenderit aspernatur quos nihil repellat aperiam. Consectetur reprehenderit tempora eos!",
+      "Delivering polished, cross-platform mobile applications tailored for optimal performance and user experience. Skilled in React Native/ Futter, I create responsive apps that seamlessly integrate with backend services and delight users with intuitive UI and smooth interactions.",
     href: "",
   },
   {
     num: "02",
     title: "Web Development",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nisi, eum, mollitia non error numquam sunt itaque magnam rerum quasi reprehenderit aspernatur quos nihil repellat aperiam. Consectetur reprehenderit tempora eos!",
+      "Building robust, responsive web applications using a variety of web technologies. From front-end UI to back-end logic, I architect scalable solutions that prioritize speed, accessibility, and clean code.",
     href: "",
   },
-  {
-    num: "03",
-    title: "UI/UX Design",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nisi, eum, mollitia non error numquam sunt itaque magnam rerum quasi reprehenderit aspernatur quos nihil repellat aperiam. Consectetur reprehenderit tempora eos!",
-    href: "",
-  },
+  // {
+  //   num: "03",
+  //   title: "UI/UX Design",
+  //   description:
+  //     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nisi, eum, mollitia non error numquam sunt itaque magnam rerum quasi reprehenderit aspernatur quos nihil repellat aperiam. Consectetur reprehenderit tempora eos!",
+  //   href: "",
+  // },
 ];
 
 const Services = () => {
@@ -42,23 +42,31 @@ const Services = () => {
               ease: "easeIn",
             },
           }}
-
           className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
         >
           {services.map((item, index) => {
             return (
-              <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
+              <div
+                key={index}
+                className="flex-1 flex flex-col justify-center gap-6 group"
+              >
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline group-hover:text-outline-hover text-transparent transition-all duration-500">{item.num}</div>
+                  <div className="text-5xl font-extrabold text-outline group-hover:text-outline-hover text-transparent transition-all duration-500">
+                    {item.num}
+                  </div>
 
-                  <Link href={item.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
-                    <BsArrowDownRight className="text-primary text-3xl"/>
+                  <Link
+                    href={item.href}
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{item.title}</h2>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {item.title}
+                </h2>
                 <p className="text-white/60">{item.description}</p>
                 <div className="border-b border-white"></div>
-          
               </div>
             );
           })}
